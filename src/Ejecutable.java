ee2060575b75fadb4ff6ea2af4f80f3599883156
diegoacuario <diegoacuario11@gmail.com>
2014-10-12 09:28:54 -0500

    Versión de la clase anterior

---
+++ src/Ejecutable.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author INTERNET3
+ */
+public class Ejecutable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showConfirmDialog(null, "Desea salir?");
+    }
+    
+}
